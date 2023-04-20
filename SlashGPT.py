a0c2816af5abd080cd76adfa02627b95f5417de8
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2023-04-19 14:34:27 -1000

    table driven

--- SlashGPT.py
+++ SlashGPT.py
@@ -16,14 +16,17 @@ print(f"Model = {OPENAI_API_MODEL}")
 openai.api_key = OPENAI_API_KEY
 
 # Reading Prompt files
+prompts = {}
 files = os.listdir("./prompts")
-print(files)
+# print(files)
 for file in files:
     key = file.split('.')[0]
     with open(f"./prompts/{file}", 'r') as f:
         data = json.load(f)
-    print(key, file, data)
+    # print(key, file, data)
+    prompts[key] = data
 
+print(prompts)
 messages = []
 
 while True:
@@ -31,36 +34,25 @@ while True:
     if (len(value) == 0):
         continue
     if (value[0] == "/"):
-        print(f"\033[92m\033[1m{value}\033[95m\033[0m")
-        if (value == "/bye"):
+        key = value[1:]
+        print(f"\033[92m\033[1m{key}\033[95m\033[0m")
+        if (key == "bye"):
             break
-        elif (value == "/reset"):
+        elif (key == "reset"):
             messages = []
             continue            
-        elif (value == "/patent"):
-            messages = [{"role":"system", "content":"""
-                You are a patent attoney, who is responsible in 
-                (1) getting novel ideas from the client,
-                (2) identify patentable ideas,
-                (3) discuss those ideas with the client in plain English,
-                (4) prepare a summary document in plain English, from which another attoney will generate the patent application.
-            """}]
-        elif (value == "/english"):
-            messages = [{"role":"system", "content":"""
-                You are a English teacher. 
-                First, always ask the native language of the student simply asking "What is your native language?", and switch the discussion to that language.
-                Second, ask for the target TOEIC level.
-            """}]
-        elif (value == "/therapy"):
-            messages = [{"role":"system", "content":"""
-                You are a therapist. Ask various questions and listen.
-            """}]
         else:
-            print("Invalid Slash command")
+            prompt = prompts[key]
+            if (prompt):
+                print(prompt)
+                messages = [{"role":"system", "content":'\n'.join(prompt["Prompt"])}]
+            else:            
+                print(f"Invalid Slash command: {key}")
     else:  
         messages.append({"role":"user", "content":value})
 
-    # print(f"{messages}")
+    print(f"{messages}")
+
     response = openai.ChatCompletion.create(model=OPENAI_API_MODEL, messages=messages, temperature=OPENAI_TEMPERATURE)
     answer = response['choices'][0]['message']
     print(f"\033[92m\033[1mGPT\033[95m\033[0m: {answer['content']}")
