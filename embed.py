bdd09d92fd36c53edd80c95d4a07ca6f8389ff01
snakajima <satoshi.nakajima@gmail.com>
2023-05-10 08:24:43 -0700

    pinecone create_index

--- embed.py
+++ embed.py
@@ -4,6 +4,7 @@ import openai  # for calling the OpenAI API
 import pandas as pd  # for storing text and embeddings data
 import tiktoken  # for counting tokens
 import os
+import pinecone
 from dotenv import load_dotenv
 from scipy import spatial  # for calculating vector similarities for search
 
@@ -12,15 +13,39 @@ load_dotenv() # Load default environment variables (.env)
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
 assert OPENAI_API_KEY, "OPENAI_API_KEY environment variable is missing from .env"
 openai.api_key = OPENAI_API_KEY
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY", "")
+assert PINECONE_API_KEY, "PINECONE_API_KEY environment variable is missing from .env"
+PINECONE_ENVIRONMENT = os.getenv("PINECONE_ENVIRONMENT", "")
+assert (
+    PINECONE_ENVIRONMENT
+), "PINECONE_ENVIRONMENT environment variable is missing from .env"
 
 # models
 EMBEDDING_MODEL = "text-embedding-ada-002"
 GPT_MODEL = "gpt-3.5-turbo"
 
+# Initialize Pinecone
+pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_ENVIRONMENT)
+
+# Create Pinecone index
+table_name = "olympic-2022"
+dimension = 1536
+metric = "cosine"
+pod_type = "p1"
+if table_name not in pinecone.list_indexes():
+    print("Creating pinecone index")
+    pinecone.create_index(
+        table_name, dimension=dimension, metric=metric, pod_type=pod_type
+    )
+
+
+print("loadig CSV file")
 embeddings_path = "./output/winter_olympics_2022.csv"
 
 df = pd.read_csv(embeddings_path)
+
 # convert embeddings from CSV str type back to list type
+print("converting it to list type")
 df['embedding'] = df['embedding'].apply(ast.literal_eval)
 # print(df)
 
