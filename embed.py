7673803468100c4f035ab469b2050406a85681df
snakajima <satoshi.nakajima@gmail.com>
2023-05-10 06:16:30 -0700

    from tutorial

---
+++ embed.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import ast  # for converting embeddings saved as strings back to arrays
+import openai  # for calling the OpenAI API
+import pandas as pd  # for storing text and embeddings data
+import tiktoken  # for counting tokens
+import os
+from dotenv import load_dotenv
+from scipy import spatial  # for calculating vector similarities for search
+
+# Configuration
+load_dotenv() # Load default environment variables (.env)
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
+assert OPENAI_API_KEY, "OPENAI_API_KEY environment variable is missing from .env"
+openai.api_key = OPENAI_API_KEY
+
+# models
+EMBEDDING_MODEL = "text-embedding-ada-002"
+GPT_MODEL = "gpt-3.5-turbo"
+
+# an example question about the 2022 Olympics
+query = 'Which athletes won the gold medal in curling at the 2022 Winter Olympics?'
+
+response = openai.ChatCompletion.create(
+    messages=[
+        {'role': 'system', 'content': 'You answer questions about the 2022 Winter Olympics.'},
+        {'role': 'user', 'content': query},
+    ],
+    model=GPT_MODEL,
+    temperature=0,
+)
+
+print(response['choices'][0]['message']['content'])
