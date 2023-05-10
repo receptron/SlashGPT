e3ba2ac7b6da373526fd39a288637f3925b31503
snakajima <satoshi.nakajima@gmail.com>
2023-05-10 06:39:42 -0700

    relatednesses

--- embed.py
+++ embed.py
@@ -17,16 +17,37 @@ openai.api_key = OPENAI_API_KEY
 EMBEDDING_MODEL = "text-embedding-ada-002"
 GPT_MODEL = "gpt-3.5-turbo"
 
-# an example question about the 2022 Olympics
-query = 'Which athletes won the gold medal in curling at the 2022 Winter Olympics?'
+embeddings_path = "./output/winter_olympics_2022.csv"
 
-response = openai.ChatCompletion.create(
-    messages=[
-        {'role': 'system', 'content': 'You answer questions about the 2022 Winter Olympics.'},
-        {'role': 'user', 'content': query},
-    ],
-    model=GPT_MODEL,
-    temperature=0,
-)
+df = pd.read_csv(embeddings_path)
+# convert embeddings from CSV str type back to list type
+df['embedding'] = df['embedding'].apply(ast.literal_eval)
+# print(df)
 
-print(response['choices'][0]['message']['content'])
+# search function
+def strings_ranked_by_relatedness(
+    query: str,
+    df: pd.DataFrame,
+    relatedness_fn=lambda x, y: 1 - spatial.distance.cosine(x, y),
+    top_n: int = 100
+) -> tuple[list[str], list[float]]:
+    """Returns a list of strings and relatednesses, sorted from most related to least."""
+    query_embedding_response = openai.Embedding.create(
+        model=EMBEDDING_MODEL,
+        input=query,
+    )
+    query_embedding = query_embedding_response["data"][0]["embedding"]
+    strings_and_relatednesses = [
+        (row["text"], relatedness_fn(query_embedding, row["embedding"]))
+        for i, row in df.iterrows()
+    ]
+    strings_and_relatednesses.sort(key=lambda x: x[1], reverse=True)
+    strings, relatednesses = zip(*strings_and_relatednesses)
+    return strings[:top_n], relatednesses[:top_n]
+
+# examples
+strings, relatednesses = strings_ranked_by_relatedness("curling gold medal", df, top_n=5)
+for string, relatedness in zip(strings, relatednesses):
+    print(f"{relatedness=:.3f}")
+    # print(string)
+
