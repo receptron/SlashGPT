8b4533784e8b4a2e6a279c1f6ba5ea13b763a37e
snakajima <satoshi.nakajima@gmail.com>
2023-06-30 14:48:14 -0700

    beautify

--- resources/browser.py
+++ resources/browser.py
@@ -1,20 +1,27 @@
-
-from termcolor import colored
+import requests
+from bs4 import BeautifulSoup
 
 def fetch(url: str):
-    return { 
-        "result":"success", 
-        "text":"""
-    Elon Musk is a business magnate and investor. He is the founder, CEO, and chief engineer of SpaceX; angel investor, CEO, and product architect of Tesla, Inc.; owner, CTO, and chairman of Twitter; founder of The Boring Company and X Corp.; co-founder of Neuralink and OpenAI; and president of the philanthropic Musk Foundation. Musk is the wealthiest person in the world according to the Bloomberg Billionaires Index and Forbes's Real Time Billionaires list as of June 2023, primarily from his ownership stakes in Tesla and SpaceX.
 
-Musk is known for his ambitious projects and his unconventional ideas. He has been criticized for his work ethic, his treatment of employees, and his sometimes erratic behavior. However, he is also admired for his vision and his ability to achieve seemingly impossible goals.
+    response = requests.get(url)
 
-Some of Musk's most notable accomplishments include:
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the HTML content using BeautifulSoup
+        soup = BeautifulSoup(response.content, 'html.parser')
 
-Co-founding PayPal, which was sold to eBay for $1.5 billion in 2002.
-Founding SpaceX, which has become the world's leading private space exploration company.
-Investing in Tesla, which has become the world's leading electric car manufacturer.
-Co-founding Neuralink, which is developing brain-computer interfaces.
-Co-founding OpenAI, which is a non-profit research company that promotes the safe development of artificial intelligence.
-Musk is a controversial figure, but there is no doubt that he is one of the most influential people in the world today. He is a visionary entrepreneur who is changing the way we think about transportation, energy, and space exploration.
-        """}
+        # Find all <p>, <div>, and <article> tags and extract their text content
+        body_text = ''
+        for tag in ['p', 'div', 'article']:
+            elements = soup.find_all(tag)
+            for element in elements:
+                body_text += element.get_text() + '\n'
+
+        # Print the extracted body text
+        return { 
+            "result":"success",
+            "text": body_text[:15000] }
+    else:    
+        return {
+            "reuslt":"error",
+        }
