141f0b8a850fb203773d9d475f4c93cc9c2be2dd
snakajima <satoshi.nakajima@gmail.com>
2023-06-30 16:42:13 -0700

    support non html file

--- resources/browser.py
+++ resources/browser.py
@@ -13,15 +13,20 @@ def fetch(url: str):
 
     # Check if the request was successful
     if response.status_code == 200:
-        # Parse the HTML content using BeautifulSoup
-        soup = BeautifulSoup(response.content, 'html.parser')
+        contentType = response.headers['Content-Type']
+        if contentType[:9] == 'text/html':
+            # Parse the HTML content using BeautifulSoup
+            soup = BeautifulSoup(response.content, 'html.parser')
 
-        # Find all <p>, <div>, and <article> tags and extract their text content
-        body_text = ''
-        for tag in ['p', 'div', 'article']:
-            elements = soup.find_all(tag)
-            for element in elements:
-                body_text += element.get_text() + '\n'
+            # Find all <p>, <div>, and <article> tags and extract their text content
+            body_text = ''
+            for tag in ['p', 'div', 'article']:
+                elements = soup.find_all(tag)
+                for element in elements:
+                    body_text += element.get_text() + '\n'
+        else:
+            print('non text/htlm')
+            body_text = response.text        
 
         num = num_tokens(body_text)
         if num >  4500:
