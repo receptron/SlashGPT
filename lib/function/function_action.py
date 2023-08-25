f38f02d6b59c3384e0e219944e6c7f3c9ff3cc6b
isamu <isamu@to-kyo.to>
2023-08-25 16:29:22 +0900

    separate network feature

--- lib/function/function_action.py
+++ lib/function/function_action.py
@@ -1,15 +1,11 @@
-import json
 import os
 import urllib.parse
 from urllib.parse import urlparse
 
-import requests
-from gql import Client, gql
-from gql.transport.requests import RequestsHTTPTransport
 from termcolor import colored
 
 from lib.utils.utils import CALL_TYPE
-
+from lib.function.network import graphQLRequest, http_request
 
 class FunctionAction:
     @classmethod
@@ -34,34 +30,12 @@ class FunctionAction:
         data = self.__get("emit_data")
         return {x: data.get(x).format(**arguments) for x in data}
 
-    def get_appkey_value(self):
-        appkey = self.__get("appkey")
-        url = self.__get("url")
-
-        if appkey:
-            appkey_value = os.getenv("SLASH_GPT_ENV_" + appkey, "")
-
-            # check domain
-            param = appkey_value.split(",")
-            if len(param) == 2:
-                parsed_url = urlparse(url)
-                if param[0] != parsed_url.netloc:
-                    print(
-                        colored(f"Invalid appkey domain {appkey} in .env file.", "red")
-                    )
-                    return
-                appkey_value = param[1]
-
-            if not appkey_value:
-                print(colored(f"Missing {appkey} in .env file.", "red"))
-            return appkey_value
-
     def call_api(self, arguments, verbose):
         type = self.__call_type()
         if type == CALL_TYPE.REST:
-            appkey_value = self.get_appkey_value() or ""
+            appkey_value = self.__get_appkey_value() or ""
 
-            return self.http_request(
+            return http_request(
                 self.__get("url"),
                 self.__get("method"),
                 self.__function_action_data.get("headers", {}),
@@ -70,7 +44,7 @@ class FunctionAction:
                 verbose,
             )
         if type == CALL_TYPE.GRAPHQL:
-            return self.graphQLRequest(self.__get("url"), arguments)
+            return graphQLRequest(self.__get("url"), arguments)
 
         if type == CALL_TYPE.DATA_URL:
             return self.read_dataURL_template(
@@ -108,43 +82,6 @@ class FunctionAction:
         if "message" in self.__function_action_data:
             return CALL_TYPE.MESSAGE_TEMPLATE
 
-    def graphQLRequest(self, url, arguments):
-        transport = RequestsHTTPTransport(url=url, use_json=True)
-        client = Client(transport=transport)
-        query = arguments.get("query")
-        graphQuery = gql(f"query {query}")
-        try:
-            response = client.execute(graphQuery)
-            return json.dumps(response)
-        except Exception as e:
-            return str(e)
-
-    def http_request(self, url, method, headers, appkey_value, arguments, verbose):
-        appkey = {"appkey": appkey_value}
-        headers = {
-            key: value.format(**arguments, **appkey) for key, value in headers.items()
-        }
-        if method == "POST":
-            headers["Content-Type"] = "application/json"
-            if verbose:
-                print(colored(f"Posting to {url} {headers}", "cyan"))
-            response = requests.post(url, headers=headers, json=arguments)
-        else:
-            if verbose:
-                print(colored(arguments.items(), "cyan"))
-            url = url.format(
-                **{key: urllib.parse.quote(value) for key, value in arguments.items()},
-                **appkey,
-            )
-            if verbose:
-                print(colored(f"Fetching from {url}", "cyan"))
-            response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            return response.text
-        else:
-            print(
-                colored(f"Got {response.status_code}:{response.text} from {url}", "red")
-            )
 
     def read_dataURL_template(
         self, template, mime_type, message_template, arguments, verbose
@@ -157,3 +94,26 @@ class FunctionAction:
             data = template.format(**arguments)
             dataURL = f"data:{_mime_type};charset=utf-8,{urllib.parse.quote_plus(data)}"
             return message_template.format(url=dataURL)
+        
+    def __get_appkey_value(self):
+        appkey = self.__get("appkey")
+        url = self.__get("url")
+
+        if appkey:
+            appkey_value = os.getenv("SLASH_GPT_ENV_" + appkey, "")
+
+            # check domain
+            param = appkey_value.split(",")
+            if len(param) == 2:
+                parsed_url = urlparse(url)
+                if param[0] != parsed_url.netloc:
+                    print(
+                        colored(f"Invalid appkey domain {appkey} in .env file.", "red")
+                    )
+                    return
+                appkey_value = param[1]
+
+            if not appkey_value:
+                print(colored(f"Missing {appkey} in .env file.", "red"))
+            return appkey_value
+
