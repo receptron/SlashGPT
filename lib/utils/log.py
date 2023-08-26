b1dcc24c17e84df50f56f3016266f51438b3ff32
isamu <isamu@to-kyo.to>
2023-08-26 10:16:27 +0900

    pretty log format

--- lib/utils/log.py
+++ lib/utils/log.py
@@ -12,4 +12,4 @@ def create_log_dir(manifest_key):
 def save_log(manifest_key, messages, time):
     timeStr = time.strftime("%Y-%m-%d %H-%M-%S.%f")
     with open(f"output/{manifest_key}/{timeStr}.json", "w") as f:
-        json.dump(messages, f, ensure_ascii=False)
+        json.dump(messages, f, ensure_ascii=False, indent=2)
