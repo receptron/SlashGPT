e471e287f7346cadb7e431668aec1b2b19f24394
isamu <isamu@to-kyo.to>
2023-09-03 04:39:50 +0900

    add bilingual-gpt-neox-4b

--- config/llm_config.py
+++ config/llm_config.py
@@ -44,6 +44,11 @@ llm_models = {
         "model_name": "rinna/japanese-gpt2-xsmall",
         "max_token": 4096,
     },
+    "rinna": {
+        "engine_name": "from_pretrained-rinna",
+        "model_name": "rinna/bilingual-gpt-neox-4b-instruction-sft",
+        "max_token": 4096,
+    },
 }
 
 llm_engine_configs = {
@@ -59,5 +64,9 @@ llm_engine_configs = {
     "from_pretrained": {
         "module_name": "plugins.engine.from_pretrained",
         "class_name": "LLMEngineFromPretrained",
+    },
+    "from_pretrained-rinna": {
+        "module_name": "plugins.engine.from_pretrained2",
+        "class_name": "LLMEngineFromPretrained2",
     },
 }
