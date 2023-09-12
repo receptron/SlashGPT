4ece24ee0bbb1592fcea72e7053823016ba159d3
isamu <isamu@to-kyo.to>
2023-09-13 07:51:14 +0900

    add data

--- prebuild.py
+++ prebuild.py
@@ -3,3 +3,4 @@ import shutil
 shutil.copytree("manifests", "src/slashgpt/manifests", dirs_exist_ok=True)
 shutil.copytree("resources", "src/slashgpt/resources", dirs_exist_ok=True)
 shutil.copytree("test", "src/slashgpt/test", dirs_exist_ok=True)
+shutil.copytree("data", "src/slashgpt/data", dirs_exist_ok=True)
