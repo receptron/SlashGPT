2db1440cdf5e5139645e87e845fe36a9600b4291
isamu <isamu@to-kyo.to>
2023-09-25 16:45:20 +0900

    env

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+from dotenv import load_dotenv
 from flask import Flask, jsonify, render_template, request
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
@@ -10,6 +11,8 @@ from slashgpt.chat_slash_config import ChatSlashConfig  # noqa: E402
 from slashgpt.function.jupyter_runtime import PythonRuntime  # noqa: E402
 from slashgpt.history.storage.file import ChatHistoryFileStorage  # noqa: E402
 from slashgpt.utils.print import print_error  # noqa: E402
+
+load_dotenv()
 
 app = Flask(__name__)
 
