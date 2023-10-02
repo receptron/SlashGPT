b7e1f08e3e8676d7a740db71aa966682d0dddbc8
snakajima <satoshi.nakajima@gmail.com>
2023-10-02 15:40:25 -0700

    spacex.json

--- tne/sample.py
+++ tne/sample.py
@@ -16,3 +16,21 @@ from slashgpt.history.storage.file import ChatHistoryFileStorage  # noqa: E402
 from slashgpt.llms.model import get_llm_model_from_key  # noqa: E402
 from slashgpt.utils.print import print_error  # noqa: E402
 
+load_dotenv()
+current_dir = os.path.dirname(__file__)
+
+config = ChatSlashConfig(current_dir, current_dir + "/manifests/sample", llm_models, llm_engine_configs)
+agent_name = "spacex"
+llm_name = "gpt3"
+manifest = config.manifests[agent_name]
+# print(manifest)
+model = get_llm_model_from_key(llm_name, config.llm_models)
+# print(model)
+
+engine = ChatHistoryFileStorage("sample", agent_name)
+session_id = engine.session_id
+print(session_id)
+# session = ChatSession(config, manifest=manifest, agent_name=agent_name, history_engine=engine)
+# session.set_llm_model(llm_name)
+# print (session_id, session, engine)
+
