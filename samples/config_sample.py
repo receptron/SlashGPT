ae3888ef161e4326ee381930bd3a22ca0a6b3688
snakajima <satoshi.nakajima@gmail.com>
2023-10-02 16:27:18 -0700

    create_session

--- samples/config_sample.py
+++ samples/config_sample.py
@@ -1,12 +1,9 @@
-import json
 import os
-import re
 import sys
 
 from dotenv import load_dotenv
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../src"))
-sys.path.append(os.path.join(os.path.dirname(__file__), "../sample"))
 
 from config.llm_config_sample import llm_engine_configs, llm_models  # noqa: E402
 from slashgpt.chat_session import ChatSession  # noqa: E402
@@ -19,6 +16,7 @@ from slashgpt.utils.print import print_error  # noqa: E402
 load_dotenv()
 current_dir = os.path.dirname(__file__)
 runtime = PythonRuntime(current_dir + "/output/notebooks")
+config = ChatSlashConfig(current_dir, current_dir + "/manifests/sample", llm_models, llm_engine_configs)
 
 def process_llm(session):
     try:
@@ -41,23 +39,25 @@ def process_llm(session):
     except Exception as e:
         print_error(f"Exception: Restarting the chat :{e}")
 
-config = ChatSlashConfig(current_dir, current_dir + "/manifests/sample", llm_models, llm_engine_configs)
-agent_name = "spacex"
-llm_name = "gpt3"
-question = "Who is the CEO of SpaceX?"
-manifest = config.manifests[agent_name]
+def create_session(agent_name: str, llm_name: str):
+    manifest = config.manifests[agent_name]
+    history = ChatHistoryFileStorage("sample", agent_name)
+    # session_id = engine.session_id
+    session = ChatSession(config, manifest=manifest, agent_name=agent_name, history_engine=history)
+    model = get_llm_model_from_key(llm_name, config.llm_models)
+    session.set_llm_model(model)
+    return (session, history)
 
-engine = ChatHistoryFileStorage("sample", agent_name)
-# session_id = engine.session_id
+def main():
+    (session, history) = create_session("spacex", "gpt3")
 
-session = ChatSession(config, manifest=manifest, agent_name=agent_name, history_engine=engine)
+    question = "Who is the CEO of SpaceX?"
+    session.append_user_question(session.manifest.format_question(question))
+    process_llm(session)
+    messages = history.messages()
+    last_message = messages[len(messages)-1]
 
-model = get_llm_model_from_key(llm_name, config.llm_models)
-session.set_llm_model(model)
-session.append_user_question(session.manifest.format_question(question))
-process_llm(session)
-messages = engine.messages()
-last_message = messages[len(messages)-1]
+    print(f"Q: {question}")
+    print(f"A: {last_message.get('content')}")
 
-print(f"Q: {question}")
-print(f"A: {last_message.get('content')}")
+main()
