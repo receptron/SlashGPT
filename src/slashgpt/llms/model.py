906c3715802bbe8621760e3591caaa9fcaf099de
isamu <isamu@to-kyo.to>
2023-10-04 07:53:25 +0900

    get_engine

--- src/slashgpt/llms/model.py
+++ src/slashgpt/llms/model.py
@@ -10,7 +10,7 @@ from slashgpt.utils.print import print_error
 class LlmModel:
     def __init__(self, llm_model_data: dict, llm_engine_configs: dict):
         self.llm_model_data = llm_model_data
-        self.set_engine(llm_engine_configs)
+        self.engine = self.get_engine(llm_engine_configs)
 
     def get(self, key: str):
         return self.llm_model_data.get(key)
@@ -39,19 +39,19 @@ class LlmModel:
         # TODO default replicate model
         return "a16z-infra/llama7b-v2-chat:a845a72bb3fa3ae298143d13efa8873a2987dbf3d49c293513cd8abf4b845a83"
 
-    def set_engine(self, llm_engine_configs: dict):
+    def get_engine(self, llm_engine_configs: dict):
         class_data = llm_engine_configs.get(self.engine_name())
 
         if class_data:
             if inspect.isclass(class_data):
-                self.engine = class_data(self)
+                return class_data(self)
             else:
                 module = importlib.import_module(class_data["module_name"])
                 my_class = getattr(module, class_data["class_name"])
-                self.engine = my_class(self)
+                return my_class(self)
         else:
             print_error("No engine name: " + self.engine_name())
-            self.engine = None
+            return None
 
     def generate_response(self, messages: List[dict], manifest: Manifest, verbose: bool):
         return self.engine.chat_completion(messages, manifest, verbose)
