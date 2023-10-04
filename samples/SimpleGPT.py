7ebdaf07ca57937ba652d82d232c535bae530963
isamu <isamu@to-kyo.to>
2023-10-05 05:24:36 +0900

    format

--- samples/SimpleGPT.py
+++ samples/SimpleGPT.py
@@ -79,6 +79,6 @@ if __name__ == "__main__":
     path = os.path.join(os.path.dirname(__file__), "../")
     config = ChatConfig(path)
     main = SimpleGPT(config, "names")
-    if len(sys.argv) == 2 and sys.argv[1] == 'test':
+    if len(sys.argv) == 2 and sys.argv[1] == "test":
         sys.exit()
     main.start()
