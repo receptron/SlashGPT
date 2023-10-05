064e91a587d9ae000ded13a823ec256397eb0c7e
snakajima <satoshi.nakajima@gmail.com>
2023-10-05 09:49:02 -0700

    removed unused import

--- src/slashgpt/chat_session.py
+++ src/slashgpt/chat_session.py
@@ -8,7 +8,6 @@ from slashgpt.dbs.pinecone import DBPinecone
 from slashgpt.history.base import ChatHistory
 from slashgpt.history.storage.abstract import ChatHisoryAbstractStorage
 from slashgpt.history.storage.memory import ChatHistoryMemoryStorage
-from slashgpt.llms.default_config import default_llm_models
 from slashgpt.llms.model import LlmModel
 from slashgpt.manifest import Manifest
 from slashgpt.utils.print import print_debug, print_error, print_warning
