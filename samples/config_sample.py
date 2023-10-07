429c67786cc0dbdb84ea201e93561a9c7e2d7a32
snakajima <satoshi.nakajima@gmail.com>
2023-10-07 15:47:40 -0700

    use session.call_loop

--- samples/config_sample.py
+++ samples/config_sample.py
@@ -14,53 +14,47 @@ from slashgpt.utils.print import print_error  # noqa: E402
 
 load_dotenv()
 current_dir = os.path.dirname(__file__)
-runtime = PythonRuntime(current_dir + "/output/notebooks")
-config = ChatConfigWithManifests(current_dir, current_dir + "/manifests/sample", llm_models, llm_engine_configs)
 
 
 class SampleApp:
-    def process_llm(self, session):
-        try:
-            (res, function_call) = session.call_llm()
+    def __init__(self):
+        agent_name = "spacex"
+        self.config = config = ChatConfigWithManifests(current_dir, current_dir + "/manifests/sample", llm_models, llm_engine_configs)
+        self.runtime = PythonRuntime(current_dir + "/output/notebooks")
+        history_engine = ChatHistoryFileStorage("sample", agent_name)
+        # session_id = engine.session_id
+        model = config.get_llm_model_from_key("gpt3")
+        manifest = config.manifests[agent_name]
+        self.session = ChatSession(config, default_llm_model=model, manifest=manifest, agent_name=agent_name, history_engine=history_engine)
 
-            if function_call:
-                runtime.create_notebook(session.llm_model.name())
-                (
-                    function_message,
-                    function_name,
-                    should_call_llm,
-                ) = function_call.process_function_call(
-                    session.history,
-                    runtime,
-                    True,
-                )
-                if should_call_llm:
-                    process_llm(session)
+    def callback(self, callback_type, data):
+        if callback_type == "emit":
+            (action_method, action_data) = data
+            # All emit methods must be processed here
+            if action_method == "switch_session":
+                self.switch_session(action_data.get("manifest"), intro=False)
+                self.query_llm(action_data.get("message"))
+        if callback_type == "function":
+            (function_name, function_message) = data
+            print(f"{function_name}: {function_message}")
+
+    def process_llm(self):
+        try:
+            self.session.call_loop(self.callback, self.config.verbose, self.runtime)
 
         except Exception as e:
             print_error(f"Exception: Restarting the chat :{e}")
 
-    @classmethod
-    def create_session(cls, agent_name: str, llm_name: str):
-        manifest = config.manifests[agent_name]
-        history = ChatHistoryFileStorage("sample", agent_name)
-        # session_id = engine.session_id
-        model = config.get_llm_model_from_key(llm_name)
-        session = ChatSession(config, default_llm_model=model, manifest=manifest, agent_name=agent_name, history_engine=history)
-        return (session, history)
-
-
     def main(self):
-        (session, history) = SampleApp.create_session("spacex", "gpt3")
-
         question = "Who is the CEO of SpaceX?"
-        session.append_user_question(session.manifest.format_question(question))
-        self.process_llm(session)
-        messages = history.messages()
+        self.session.append_user_question(self.session.manifest.format_question(question))
+        self.process_llm()
+        messages = self.session.history.messages()
         last_message = messages[len(messages) - 1]
 
         print(f"Q: {question}")
         print(f"A: {last_message.get('content')}")
+
 
 app = SampleApp()
 app.main()
