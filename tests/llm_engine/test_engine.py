422b8ea64a21c528f79f82ff219fb83643c30569
snakajima <satoshi.nakajima@gmail.com>
2023-10-12 08:52:13 -0700

    ChatSession

--- tests/llm_engine/test_engine.py
+++ tests/llm_engine/test_engine.py
@@ -1,15 +1,21 @@
 import os
 import sys
 from typing import List
-
+from dotenv import load_dotenv
 import pytest
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../../src"))
 
 from slashgpt.llms.engine.base import LLMEngineBase  # noqa: E402
-from slashgpt.manifest import Manifest
+from slashgpt.manifest import Manifest  # noqa: E402
 
-class MyLlmENgine(LLMEngineBase):
+from slashgpt.chat_config import ChatConfig  # noqa: E402
+from slashgpt.chat_session import ChatSession  # noqa: E402
+
+load_dotenv()
+current_dir = os.path.dirname(__file__)
+
+class MyLlmEngine(LLMEngineBase):
     def __init__(self, llm_model):
         self.llm_model = llm_model
         return
@@ -22,9 +28,31 @@ class MyLlmENgine(LLMEngineBase):
 
 @pytest.fixture
 def engine():
-    return MyLlmENgine({})
+    return MyLlmEngine({})
 
 def test_foo(engine):
     (role, res, function_call) = engine.chat_completion([], None, False)
     assert role == "assistant"
+    assert res == "hello"
+
+res = "foo"
+
+def process_event(callback_type, data):
+    if callback_type == "bot":
+        global res
+        res = data
+
+manifest = {
+    "prompt": "abc"
+}
+
+def test_bar(engine):
+    config = ChatConfig(current_dir)
+    session = ChatSession(
+        config,
+        manifest=manifest,
+        agent_name="foo"
+    )
+    session.append_user_question("Hello")
+    session.call_loop(process_event)
     assert res == "hello"
