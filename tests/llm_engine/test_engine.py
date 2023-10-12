3409829f913229bae2a483f7d1a6c8b15d510dcf
snakajima <satoshi.nakajima@gmail.com>
2023-10-12 05:50:45 -0700

    check res

--- tests/llm_engine/test_engine.py
+++ tests/llm_engine/test_engine.py
@@ -9,7 +9,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "../../src"))
 from slashgpt.llms.engine.base import LLMEngineBase  # noqa: E402
 from slashgpt.manifest import Manifest
 
-class MyTestEngine(LLMEngineBase):
+class MyLlmENgine(LLMEngineBase):
     def __init__(self, llm_model):
         self.llm_model = llm_model
         return
@@ -22,8 +22,9 @@ class MyTestEngine(LLMEngineBase):
 
 @pytest.fixture
 def engine():
-    return MyTestEngine({})
+    return MyLlmENgine({})
 
 def test_foo(engine):
     (role, res, function_call) = engine.chat_completion([], None, False)
     assert role == "assistant"
+    assert res == "hello"
