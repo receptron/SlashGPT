97e86b5e4ab924338ce580f25b9d8ea3f225053f
isamu <isamu@to-kyo.to>
2023-10-14 06:26:32 +0900

    fix lint error

--- src/slashgpt/chat_session.py
+++ src/slashgpt/chat_session.py
@@ -99,8 +99,6 @@ class ChatSession:
         embeddings = self.manifest.get("embeddings")
         if embeddings:
             table_name = embeddings.get("name")
-            metadata = embeddings.get("metadata")
-            storage_id = metadata.get("storage_id") if metadata else ""
 
             try:
                 if embeddings["db_type"] == "pinecone":
