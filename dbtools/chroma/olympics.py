b81bb30060e1c7c778289a83c627dcde979e96b3
snakajima <satoshi.nakajima@gmail.com>
2023-10-14 06:32:19 -0700

    olympics

---
+++ dbtools/chroma/olympics.py
@@ -0,0 +1,65 @@
+import os
+import ast  # for converting embeddings saved as strings back to arrays
+import openai  # for calling the OpenAI API
+import pandas as pd  # for storing text and embeddings data
+import tiktoken  # for counting tokens
+import chromadb
+import numpy as np
+# from scipy import spatial  # for calculating vector similarities for search
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY", None)
+EMBEDDING_MODEL = "text-embedding-ada-002"
+
+# download pre-chunked text and pre-computed embeddings
+# this file is ~200 MB, so may take a minute depending on your connection speed
+# embeddings_path = "https://cdn.openai.com/API/examples/data/winter_olympics_2022.csv"
+#embeddings_path = "./testdata/winter_olympics_2022.csv"
+embeddings_path = "./testdata/foo.csv"
+
+print("loading... (takes a while)")
+df = pd.read_csv(embeddings_path)
+
+# convert embeddings from CSV str type back to list type
+df['embedding'] = df['embedding'].apply(ast.literal_eval)
+
+# the dataframe has two columns: "text" and "embedding"
+# print(df)
+
+
+db_path = os.path.normpath(os.path.expanduser("~/.slashgpt/chroma-db"))
+
+client = chromadb.PersistentClient(path=db_path)
+
+collection = client.get_or_create_collection("olympic2022")
+
+for i, row in df.iterrows():
+    query_embedding_response = openai.Embedding.create(
+        model=EMBEDDING_MODEL,
+        input=row['text'],
+    )
+
+    collection.upsert(
+        ids=[str(i)],
+        embeddings=row['embedding'],
+        metadatas=[
+            {"id": i},
+        ],
+        documents=[row['text']],
+    )
+
+q = "Sharipzyanov"
+query_embedding_response = openai.Embedding.create(
+    model=EMBEDDING_MODEL,
+    input=q,
+)
+
+res = collection.query(
+    query_embeddings=[np.array(query_embedding_response["data"][0]["embedding"]).tolist()],
+    n_results=1,
+)
+
+print(list(map(lambda x: "".join(x), list(*res["documents"]))))
