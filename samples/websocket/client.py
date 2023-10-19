fcfc41d518600286b887ba7f63617fbed27c4a08
snakajima <satoshi.nakajima@gmail.com>
2023-10-19 09:00:21 -1000

    step into group chat

--- samples/websocket/client.py
+++ samples/websocket/client.py
@@ -5,6 +5,8 @@ async def main():
     async with websockets.connect("ws://localhost:8765") as websocket:
         while True:
             question = input("you:")
+            if question == "/bye":
+                return
             await websocket.send(question)
             message = await websocket.recv()
             print("message:", message)
--- samples/websocket/server.py
+++ samples/websocket/server.py
@@ -1,11 +1,23 @@
 import asyncio
 import websockets
 
+connected_clients = set()
+
 async def handle_websocket(websocket, path):
-    while True:
-        message = await websocket.recv()
-        print("message:", message)
-        await websocket.send(f"replying to {message}")
+    connected_clients.add(websocket)
+    print(f"Connected {path}")
+
+    try:
+        while True:
+            message = await websocket.recv()
+            print("message:", message)
+            await websocket.send(f"replying to {message}")
+    except websockets.exceptions.ConnectionClosed:
+        # Connection was lost, remove client
+        print("Connection closed {path}")
+    finally:
+        # Make sure to remove the client in any case to prevent memory leaks
+        connected_clients.remove(websocket)
 
 async def main():
     async with websockets.serve(handle_websocket, "localhost", 8765):
