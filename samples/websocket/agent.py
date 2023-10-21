892c57a477a921d1b552fc66a020b9d52df9459a
snakajima <satoshi.nakajima@gmail.com>
2023-10-21 08:11:39 -1000

    agent.py uses ChatSession to respond

--- samples/websocket/agent.py
+++ samples/websocket/agent.py
@@ -1,13 +1,31 @@
+import os
+import sys
 import asyncio
 import websockets
+from dotenv import load_dotenv
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../src"))
+from slashgpt.chat_config import ChatConfig  # noqa: E402
+from slashgpt.chat_session import ChatSession  # noqa: E402
+
+load_dotenv()
+current_dir = os.path.dirname(__file__)
+config = ChatConfig(current_dir)
+
+manifest = {
+    "title": "AI Agent on Websocket"
+}
 
 async def main():
+    print(f"Activating {manifest.get('title')}")
     async with websockets.connect("ws://localhost:8765") as websocket:
         while True:
             question = await websocket.recv()
             print("message:", question)
-            await websocket.send(f"replying to {question}")
+            session = ChatSession(config, manifest=manifest)
+            session.append_user_question(question)
+            (message, _function_call) = session.call_llm()
+            await websocket.send(message)
 
-print("foo")
 if __name__ == "__main__":
     asyncio.run(main())
--- samples/websocket/client.py
+++ samples/websocket/client.py
@@ -4,13 +4,12 @@ import websockets
 async def main():
     async with websockets.connect("ws://localhost:8765") as websocket:
         while True:
-            question = input("you:")
+            question = input("you: ")
             if question == "/bye":
                 return
             await websocket.send(question)
             message = await websocket.recv()
-            print("message:", message)
+            print("agent:", message)
 
-print("foo")
 if __name__ == "__main__":
     asyncio.run(main())
